723bf4d05845e00364ae33430e326d212a20d3c1
Axel Olsson <axel.olsson@gmail.com>
2021-12-15 00:15:47 +0100

    Simplfy by storing all possible combinations instead of a list

--- Day14.c
+++ Day14.c
@@ -1,20 +1,12 @@
 #include "Helpers.c"
 
 #define TEMPLATE_CAP 32
-#define RULES_CAP 100
-#define PAIRS_CAP 100
 
-typedef struct {
-    uint8_t p0;
-    uint8_t p1;
-    int64_t n;
-} Pair;
-
-static int parse(const char *input, uint8_t template[TEMPLATE_CAP], uint8_t rules[RULES_CAP][RULES_CAP]) {
+static int parse(const char *input, uint8_t template[TEMPLATE_CAP], uint8_t rules[26][26]) {
     int charsRead = 0;
 
     assert(sscanf(input, "%31s\n%n", template, &charsRead) != EOF);
-
+    int n = charsRead - 2;
     input += charsRead;
 
     uint8_t p0 = 0;
@@ -24,103 +16,74 @@ static int parse(const char *input, uint8_t template[TEMPLATE_CAP], uint8_t rule
     while (sscanf(input, "%c%c -> %c\n%n", &p0, &p1, &r, &charsRead) == 3) {
         assert(p0 <= 'Z' && p1 <= 'Z' && r <= 'Z');
 
-        rules[p0][p1] = r;
+        rules[p0 - 'A'][p1 - 'A'] = r - 'A';
 
         input += charsRead;
     }
 
-    return strlen((char *)template);
+    return n;
 }
 
-static Pair *findPair(int nPairs, Pair pairs[nPairs], char p0, char p1) {
-    for (int i = 0; i < nPairs; ++i) {
-        if (pairs[i].p0 == p0 && pairs[i].p1 == p1) {
-            return &pairs[i];
-        }
-    }
-    return NULL;
-}
+static int64_t pairFromTemplate(int n, const uint8_t template[n], const uint8_t rules[26][26], int steps) {
+    // Allocate stack memory for all possible pair combinations for all steps.
+    int64_t pairsByStep[steps + 1][26][26];
+    memset(pairsByStep, 0, sizeof(int64_t) * 26 * 26 * (steps + 1));
 
-static int insertPair(int nPairs, Pair pairs[nPairs], uint8_t p0, uint8_t p1, int64_t n) {
-    Pair *existingPair = findPair(nPairs, pairs, p0, p1);
-
-    if (existingPair == NULL) {
-        pairs[nPairs++] = (Pair){p0, p1, n};
-        assert(nPairs < PAIRS_CAP);
-    } else {
-        existingPair->n += n;
-    }
-
-    return nPairs;
-}
-
-static int64_t pairFromTemplate(int n, const uint8_t template[n], const uint8_t rules[RULES_CAP][RULES_CAP], int steps) {
-    Pair pairs1[PAIRS_CAP] = {0};
-    Pair pairs2[PAIRS_CAP] = {0};
-
-    Pair *prevPairs = pairs1;
-    Pair *nextPairs = pairs2;
-    int nPrev = 0;
-    int nNext = 0;
-
-    // Insert initial pairs from template into nextPairs.
+    // Insert initial pairs from template into step 0.
     for (int i = 0; i < n - 1; ++i) {
-        nNext = insertPair(nNext, nextPairs, template[i], template[i + 1], 1);
+        ++pairsByStep[0][template[i] - 'A'][template[i + 1] - 'A'];
     }
 
-    for (int step = 0; step < steps; ++step) {
-        // Flip nextPairs pointer with prevPairs.
-        if (prevPairs == pairs2) {
-            prevPairs = pairs1;
-            nextPairs = pairs2;
-        } else {
-            prevPairs = pairs2;
-            nextPairs = pairs1;
-        }
+    for (int step = 1; step <= steps; ++step) {
+        // For every pair possibility, if pair exist from previous step, insert new pairs by rules.
+        for (int p0 = 0; p0 < 26; ++p0) {
+            for (int p1 = 0; p1 < 26; ++p1) {
+                if (pairsByStep[step - 1][p0][p1] > 0) {
+                    uint8_t insert = rules[p0][p1];
+                    assert(insert);
 
-        nPrev = nNext;
-        nNext = 0;
+                    // p0 and insert pair
+                    pairsByStep[step][p0][insert] += pairsByStep[step - 1][p0][p1];
 
-        // Consider every previous pair and insert new pairs based on rules.
-        // Pass along the previous pair count on insertion as we will do the "same" insertion for every pair count.
-        for (int i = 0; i < nPrev; ++i) {
-            uint8_t insert = rules[prevPairs[i].p0][prevPairs[i].p1];
-
-            if (insert) {
-                nNext = insertPair(nNext, nextPairs, prevPairs[i].p0, insert, prevPairs[i].n);
-                nNext = insertPair(nNext, nextPairs, insert, prevPairs[i].p1, prevPairs[i].n);
-            } else {
-                nNext = insertPair(nNext, nextPairs, prevPairs[i].p0, prevPairs[i].p1, prevPairs[i].n);
+                    // insert and p1 pair
+                    pairsByStep[step][insert][p1] += pairsByStep[step - 1][p0][p1];
+                }
             }
         }
     }
 
-    int64_t counts[RULES_CAP] = {0};
+    int64_t twiceCounts[26] = {0};
 
-    // Include last character in the count as it isn't counted twice when starting from first char in pair.
-    counts[template[n - 1]] = 1;
+    // All but first and last character are counted twice due to insert rules.
+    twiceCounts[template[0] - 'A'] = 1;
+    twiceCounts[template[n - 1] - 'A'] = 1;
 
-    // Only count first character of the pair as we have doubled them.
-    for (int i = 0; i < nNext; ++i) {
-        counts[nextPairs[i].p0] += nextPairs[i].n;
+    for (int i = 0; i < 26; ++i) {
+        for (int j = 0; j < 26; ++j) {
+            twiceCounts[i] += pairsByStep[steps][i][j];
+            twiceCounts[j] += pairsByStep[steps][i][j];
+        }
     }
 
-    int64_t max = 0;
     int64_t min = INT64_MAX;
+    int64_t max = 0;
 
-    for (int i = 0; i < RULES_CAP; ++i) {
-        max = counts[i] > max ? counts[i] : max;
-        min = counts[i] > 0 && counts[i] < min ? counts[i] : min;
+    for (int i = 0; i < 26; ++i) {
+        if (twiceCounts[i] > 0) {
+            int64_t count = twiceCounts[i] / 2;
+            min = count < min ? count : min;
+            max = count > max ? count : max;
+        }
     }
 
     return max - min;
 }
 
-static int64_t partOne(int n, const uint8_t template[n], const uint8_t rules[RULES_CAP][RULES_CAP]) {
+static int64_t partOne(int n, const uint8_t template[n], const uint8_t rules[26][26]) {
     return pairFromTemplate(n, template, rules, 10);
 }
 
-static int64_t partTwo(int n, const uint8_t template[n], const uint8_t rules[RULES_CAP][RULES_CAP]) {
+static int64_t partTwo(int n, const uint8_t template[n], const uint8_t rules[26][26]) {
     return pairFromTemplate(n, template, rules, 40);
 }
 
@@ -128,7 +91,7 @@ int main() {
     const char *input = Helpers_readInputFile(__FILE__);
 
     uint8_t template[TEMPLATE_CAP] = {0};
-    uint8_t rules[RULES_CAP][RULES_CAP] = {0};
+    uint8_t rules[26][26] = {0};
     int n = parse(input, template, rules);
 
     Helpers_assert(PART1, Helpers_clock(),
