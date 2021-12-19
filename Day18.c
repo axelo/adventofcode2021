2091f7d7a8cf996d8c8b4ad58f40009ba1143349
Axel Olsson <axel.olsson@gmail.com>
2021-12-19 15:10:20 +0100

    WIP

--- Day18.c
+++ Day18.c
@@ -1,80 +1,71 @@
 #include "Helpers.c"
 
-#define CAP 100
+#define NUMBERS_CAP 100
 #define NODES_CAP 30000
 
-// TODO: Use index pointers instead of pointers.
-
-typedef struct NodeType {
-    int value;
-    struct NodeType *left;
-    struct NodeType *right;
-    struct NodeType *parent;
-    int id;
+typedef struct {
+    uint16_t parent;
+    uint16_t left;
+    uint16_t right;
+    uint16_t value;
 } Node;
 
-Node nodes[NODES_CAP] = {0};
-int n = 0;
-int nRemoved = 0;
+typedef struct {
+    int n;
+    Node ns[NODES_CAP];
+} Nodes;
 
-static int parse(const char *input, Node *numbers[CAP]) {
+static int parse(const char *input, Nodes *nodes, int numbers[NUMBERS_CAP]) {
     int nNumbers = 0;
-    n = 0;
+    nodes->n = 1;
 
-    memset(nodes, 0, sizeof(nodes));
+    int current = nodes->n;
 
     while (*input != 0) {
-        assert(nNumbers < CAP);
+        numbers[nNumbers++] = nodes->n;
+        assert(nNumbers <= NUMBERS_CAP);
 
-        Node *p = &nodes[n++];
-        p->value = 0xdeadbeef;
-        p->id = n - 1;
+        current = nodes->n;
 
-        assert(n < NODES_CAP);
-
-        numbers[nNumbers++] = p;
+        ++nodes->n;
+        assert(nodes->n < NODES_CAP);
 
         while (*input != 0 && *input != '\n') {
             char c = *(input++);
-            assert(p != NULL && "Unbalanced snailfish number");
-            // printf("%c - %d\n", c, n);
+            assert(current != 0 && "Unbalanced snailfish number");
 
             if (c == '[') {
-                assert(p->left == NULL && "Left already occupied");
+                assert(nodes->ns[current].left == 0 && "Left already occupied");
 
-                nodes[n].parent = p;
-                p->left = &nodes[n++];
-                p = p->left;
-                p->id = n - 1;
-                p->value = -1;
+                int left = nodes->n++;
+                nodes->ns[left].parent = current;
+
+                nodes->ns[current].left = left;
+
+                current = left;
             } else if (c >= '0' && c <= '9') {
-                char d = *input;
+                nodes->ns[current].value = c - '0';
 
-                if (d >= '0' && d <= '9') {
-                    p->value = (c - '0') * 10 + (d - '0');
-                    ++input;
-                } else {
-                    p->value = c - '0';
-                }
-
-                p = p->parent;
+                current = nodes->ns[current].parent;
             } else if (c == ',') {
-                assert(p->right == NULL && "Right already occupied");
+                assert(nodes->ns[current].right == 0 && "Right already occupied");
 
-                nodes[n].parent = p;
-                p->right = &nodes[n++];
-                p = p->right;
-                p->id = n - 1;
-                p->value = -1;
+                int right = nodes->n++;
+                nodes->ns[right].parent = current;
+
+                nodes->ns[current].right = right;
+
+                current = right;
             } else if (c == ']') {
-                assert(p->left != NULL && p->right != NULL && "Missing pair");
-                p = p->parent;
+                assert(nodes->ns[current].left != 0 && nodes->ns[current].right != 0 && "Missing pair");
+
+                current = nodes->ns[current].parent;
             } else {
                 assert(false && "Unknown character");
             }
         }
 
-        assert(p == NULL && "Unbalanced snailfish number");
+        assert(current == 0 && "Unbalanced snailfish number");
 
         ++input;
     }
@@ -82,319 +73,210 @@ static int parse(const char *input, Node *numbers[CAP]) {
     return nNumbers;
 }
 
-static Node *findLeftMostValue(Node *node) {
-    bool triedLeft[NODES_CAP] = {false};
-    bool triedRight[NODES_CAP] = {false};
+static int addition(int ia, int ib, Nodes *nodes) {
+    int root = nodes->n++;
 
-    while (node != NULL) {
-        if (node->left == NULL || node->right == NULL) {
-            return node;
-        } else {
-            if (triedLeft[node->id] && triedRight[node->id]) {
-                node = node->parent;
-            } else if (!triedLeft[node->id]) {
-                triedLeft[node->id] = true;
-                node = node->left;
-            } else {
-                triedRight[node->id] = true;
-                node = node->right;
-            }
-        }
-    }
+    nodes->ns[root].parent = 0;
+    nodes->ns[root].left = ia;
+    nodes->ns[root].right = ib;
 
-    return NULL;
-}
-
-static Node *findLeftMostValue2(Node *node) {
-    bool triedLeft[NODES_CAP] = {false};
-    bool triedRight[NODES_CAP] = {false};
-
-    while (node != NULL) {
-        if (node->left == NULL || node->right == NULL) {
-            return node;
-        } else {
-            if (triedLeft[node->id] && triedRight[node->id]) {
-                node = node->parent;
-            } else if (!triedRight[node->id]) {
-                triedRight[node->id] = true;
-                node = node->right;
-            } else {
-                triedLeft[node->id] = true;
-                node = node->left;
-            }
-        }
-    }
-
-    return NULL;
-}
-
-static bool explode(Node *root) {
-    Node *c = root;
-    int level = 0;
-
-    bool triedLeft[NODES_CAP] = {false};
-    bool triedRight[NODES_CAP] = {false};
-
-    while (c != NULL) {
-        assert((c->left != NULL && c->right != NULL) || (c->left == NULL && c->right == NULL));
-
-        // printf("At level: %d\n", level);
-
-        if (c->left == NULL || c->right == NULL) {
-            // printf("Not a pair, backup up, %d\n", c->value);
-
-            if (level <= 0) {
-                printf("Nothing more to backup too\n");
-                return false;
-            }
-
-            --level;
-            // c = parents[level];
-            c = c->parent;
-        } else {
-            if (level >= 4) {
-                // printf("Woop, deep enough!\n");
-                break;
-            } else {
-                if (triedLeft[c->id] && triedRight[c->id]) {
-                    // printf("Already been here once, id: %d!\n", c->id);
-
-                    --level;
-                    // c = parents[level];
-                    c = c->parent;
-                    // return 2;
-                } else if (!triedLeft[c->id]) {
-                    // printf("Going left \n");
-                    // parents[level] = c;
-                    triedLeft[c->id] = true;
-                    ++level;
-                    c = c->left;
-                } else {
-                    // printf("Going right \n");
-                    // parents[level] = c;
-                    triedRight[c->id] = true;
-
-                    ++level;
-                    c = c->right;
-                }
-            }
-        }
-    }
-
-    if (c == NULL) {
-        // printf("Found no nested pairs\n");
-        return false;
-    }
-
-    // printf("Found node id: %d, at level: %d\n", c->id, level);
-
-    int leftNumber = c->left->value;
-    int rightNumber = c->right->value;
-
-    assert(leftNumber != -1337);
-    assert(rightNumber != -1337); // Understand why deeper than 4 (after split).
-
-    // printf("Left number %d, right number: %d, id: %d\n", leftNumber, rightNumber, c->id);
-
-    // Find left most number from c
-    Node *leftMostC = c->parent;
-    Node *prev = c;
-
-    while (leftMostC != NULL) {
-        if (leftMostC->left == NULL) {
-            // printf("Found left most value: %d\n", leftMostC->value);
-            break;
-        } else if (leftMostC->left == prev) {
-            prev = leftMostC;
-            leftMostC = leftMostC->parent;
-
-        } else {
-            // prev = leftMostC;
-            // leftMostC = leftMostC->left;
-            leftMostC = findLeftMostValue2(leftMostC->left);
-            break;
-        }
-    }
-
-    if (leftMostC != NULL) {
-        // printf("Left most c value: %d, id: %d\n", leftMostC->value, leftMostC->parent->id);
-
-        leftMostC->value += leftNumber;
-    } else {
-        // printf("Found no left most value\n");
-    }
-
-    // Find right most number from c
-    Node *rightMostC = c->parent;
-    prev = c;
-
-    while (rightMostC != NULL) {
-        if (rightMostC->right == NULL) {
-            // printf("Found right most value: %d\n", rightMostC->value);
-            break;
-        } else if (rightMostC->right == prev) {
-            prev = rightMostC;
-            rightMostC = rightMostC->parent;
-        } else {
-            // prev = rightMostC;
-            // rightMostC = rightMostC->right;
-            rightMostC = findLeftMostValue(rightMostC->right);
-            break;
-        }
-    }
-
-    if (rightMostC != NULL) {
-        // printf("Right most c value: %d, id: %d\n", rightMostC->value, rightMostC->parent->id);
-        rightMostC->value += rightNumber;
-    } else {
-        // printf("Found no right most value\n");
-    }
-
-    c->left = NULL;
-    c->right = NULL;
-    c->value = 0;
-
-    nRemoved += 2;
-
-    return true;
-}
-
-static Node *addition(Node *nodeA, Node *nodeB) {
-    Node *root = &nodes[n++];
-    root->value = 0xdeadbeef;
-    root->id = n - 1;
-
-    root->left = nodeA;
-    root->right = nodeB;
-
-    nodeA->value = -1;
-    nodeA->parent = root;
-
-    nodeB->value = -1;
-    nodeB->parent = root;
+    nodes->ns[ia].parent = root;
+    nodes->ns[ib].parent = root;
 
     return root;
 }
 
-static bool split(Node *node) {
-    bool triedLeft[NODES_CAP] = {false};
-    bool triedRight[NODES_CAP] = {false};
+static int findLeftMostNumberNode(int i, const Nodes *nodes) {
+    if (i < 0) {
+        return -1;
+    } else if (nodes->ns[i].left == 0) {
+        return i;
+    } else {
+        int left = findLeftMostNumberNode(nodes->ns[i].left, nodes);
 
-    Node *found = NULL;
+        return left != -1
+                   ? left
+                   : findLeftMostNumberNode(nodes->ns[i].right, nodes);
+    }
+}
 
-    while (node != NULL) {
-        if (node->left == NULL || node->right == NULL) {
-            if (node->value >= 10) {
-                found = node;
+static int findRightMostNumberNode(int i, const Nodes *nodes) {
+    if (i < 0) {
+        return -1;
+    } else if (nodes->ns[i].left == 0) {
+        return i;
+    } else {
+        int right = findRightMostNumberNode(nodes->ns[i].right, nodes);
+
+        return right != -1
+                   ? right
+                   : findRightMostNumberNode(nodes->ns[i].left, nodes);
+    }
+}
+
+static int findNodeToExplode(int i, int level, const Nodes *nodes) {
+    if (i < 0 || nodes->ns[i].left == 0) {
+        return -1;
+    } else if (level >= 4) {
+        return i;
+    } else {
+        int left = findNodeToExplode(nodes->ns[i].left, level + 1, nodes);
+
+        return left != -1
+                   ? left
+                   : findNodeToExplode(nodes->ns[i].right, level + 1, nodes);
+    }
+}
+
+static bool explode(int i, Nodes *nodes) {
+    int j = findNodeToExplode(i, 0, nodes);
+
+    if (j == -1) {
+        return false;
+    } else {
+        int closestParentWithLeft = nodes->ns[j].parent;
+        int prevChild = j;
+        int rightMostNumberNode = -1;
+
+        while (closestParentWithLeft != 0) {
+            if (nodes->ns[closestParentWithLeft].left == prevChild) {
+                prevChild = closestParentWithLeft;
+                closestParentWithLeft = nodes->ns[closestParentWithLeft].parent;
+            } else {
+                rightMostNumberNode = findRightMostNumberNode(nodes->ns[closestParentWithLeft].left, nodes);
                 break;
-            } else {
-                node = node->parent;
-            }
-        } else {
-            if (triedLeft[node->id] && triedRight[node->id]) {
-                node = node->parent;
-            } else if (!triedLeft[node->id]) {
-                triedLeft[node->id] = true;
-                node = node->left;
-            } else {
-                triedRight[node->id] = true;
-                node = node->right;
             }
         }
+
+        int closestParentWithRight = nodes->ns[j].parent;
+        prevChild = j;
+        int leftMostNumberNode = -1;
+
+        while (closestParentWithRight != 0) {
+            if (nodes->ns[closestParentWithRight].right == 0) {
+                break;
+            } else if (nodes->ns[closestParentWithRight].right == prevChild) {
+                prevChild = closestParentWithRight;
+                closestParentWithRight = nodes->ns[closestParentWithRight].parent;
+            } else {
+                leftMostNumberNode = findLeftMostNumberNode(nodes->ns[closestParentWithRight].right, nodes);
+                break;
+            }
+        }
+
+        if (rightMostNumberNode != -1) {
+            nodes->ns[rightMostNumberNode].value += nodes->ns[nodes->ns[j].left].value;
+        }
+
+        if (leftMostNumberNode != -1) {
+            nodes->ns[leftMostNumberNode].value += nodes->ns[nodes->ns[j].right].value;
+        }
+
+        nodes->ns[j].left = 0;
+        nodes->ns[j].right = 0;
+        nodes->ns[j].value = 0;
+
+        return true;
     }
+}
 
-    if (found != NULL) {
-        // printf("Found node to split, %d: %d\n", found->id, found->value);
+static int findNodeToSplit(int i, const Nodes *nodes) {
+    if (i <= 0) {
+        return -1;
+    } else if (nodes->ns[i].left == 0) {
+        return nodes->ns[i].value >= 10 ? i : -1;
+    } else {
+        int left = findNodeToSplit(nodes->ns[i].left, nodes);
 
-        assert(found->value != -1337);
+        return left != -1 ? left : findNodeToSplit(nodes->ns[i].right, nodes);
+    }
+}
 
-        Node *left = &nodes[n++];
-        left->id = n - 1;
-        left->value = found->value / 2;
-        left->parent = found;
+static bool split(int i, Nodes *nodes) {
+    int j = findNodeToSplit(i, nodes);
 
-        assert(n < NODES_CAP);
+    if (j != -1) {
+        int left = nodes->n++;
+        nodes->ns[left].parent = j;
+        nodes->ns[left].value = nodes->ns[j].value / 2;
 
-        Node *right = &nodes[n++];
-        right->id = n - 1;
-        right->value = ((float)found->value / 2) + 0.5;
-        right->parent = found;
+        assert(nodes->n < NODES_CAP);
 
-        assert(n < NODES_CAP);
+        int right = nodes->n++;
+        nodes->ns[right].parent = j;
+        nodes->ns[right].value = ((float)nodes->ns[j].value / 2) + 0.5;
 
-        found->left = left;
-        found->right = right;
-        found->value = -1337;
+        assert(nodes->n < NODES_CAP);
+
+        nodes->ns[j].left = left;
+        nodes->ns[j].right = right;
 
         return true;
     } else {
-        // printf("Found no node to split\n");
         return false;
     }
 }
 
-static void reduce(Node *number) {
+static void reduce(int i, Nodes *nodes) {
     do {
-        while (explode(number)) {
+        while (explode(i, nodes)) {
         }
-    } while (split(number));
+    } while (split(i, nodes));
 }
 
-static int magnitude(const Node *node) {
-    return node->left == NULL || node->right == NULL
-               ? node->value
-               : 3 * magnitude(node->left) + 2 * magnitude(node->right);
+static int magnitude(int i, const Nodes *nodes) {
+    return nodes->ns[i].left == 0
+               ? nodes->ns[i].value
+               : 3 * magnitude(nodes->ns[i].left, nodes) + 2 * magnitude(nodes->ns[i].right, nodes);
 }
 
-static void dump(const Node *node, int level) {
-    if (node->left == NULL || node->right == NULL) {
-        printf("%d", node->value);
+static void dump(int i, const Nodes *nodes) {
+    if (nodes->ns[i].left == 0) {
+        printf("%d", nodes->ns[i].value);
     } else {
         printf("[");
-        dump(node->left, level + 1);
+        dump(nodes->ns[i].left, nodes);
         printf(",");
-        dump(node->right, level + 1);
-        // printf("] <%d|%d>", level, node->id);
-        // printf("] %d", node->id);
+        dump(nodes->ns[i].right, nodes);
         printf("]");
     }
 
-    if (node->value == (int)0xdeadbeef) {
+    if (nodes->ns[i].parent == 0) {
         printf("\n");
     }
 }
 
-static int partOne(int nNumbers, Node *numbers[CAP]) {
-    Node *number = numbers[0];
+static int partOne(const Nodes *nodes, int n, int numbers[n]) {
+    Nodes ns;
+    memcpy(&ns, nodes, sizeof(nodes->n) + nodes->n * sizeof(nodes->ns[0]));
 
-    for (int i = 1; i < nNumbers; ++i) {
-        number = addition(number, numbers[i]);
-        reduce(number);
+    int number = numbers[0];
+
+    for (int i = 1; i < n; ++i) {
+        number = addition(number, numbers[i], &ns);
+        reduce(number, &ns);
     }
 
-    dump(number, 0);
+    dump(number, &ns);
 
-    int m = magnitude(number);
-
-    printf("nodes: %d, nodes removed: %d\n", n, nRemoved);
-
-    return m;
+    return magnitude(number, &ns);
 }
 
-static int partTwo(int nNumbers123, const char *input) {
+static int partTwo(const Nodes *nodes, int n, int numbers[n]) {
+    int nsSize = (sizeof(Nodes) / NODES_CAP + 1) * nodes->n;
+    Nodes ns;
+
     int largestMagnitude = INT_MIN;
 
-    for (int i = 0; i < nNumbers123; ++i) {
-        for (int j = 0; j < nNumbers123; ++j) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
             if (i != j) {
+                memcpy(&ns, nodes, nsSize);
 
-                Node *numbers[CAP];
-                parse(input, numbers);
+                int number = addition(numbers[i], numbers[j], &ns);
+                reduce(number, &ns);
 
-                Node *number = addition(numbers[i], numbers[j]);
-                reduce(number);
-
-                int m = magnitude(number);
+                int m = magnitude(number, &ns);
 
                 if (m > largestMagnitude) {
                     largestMagnitude = m;
@@ -409,15 +291,16 @@ static int partTwo(int nNumbers123, const char *input) {
 int main() {
     const char *input = Helpers_readInputFile(__FILE__);
 
-    Node *numbers[CAP] = {NULL};
-    int nNumbers = parse(input, numbers);
+    Nodes nodes = {0};
+    int numbers[NUMBERS_CAP] = {0};
+    int n = parse(input, &nodes, numbers);
 
     Helpers_assert(PART1, Helpers_clock(),
-                   partOne(nNumbers, numbers),
+                   partOne(&nodes, n, numbers),
                    4140, 4289);
 
     Helpers_assert(PART2, Helpers_clock(),
-                   partTwo(nNumbers, input),
+                   partTwo(&nodes, n, numbers),
                    3993, 4807);
 
     return 0;
