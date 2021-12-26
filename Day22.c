6c40f3540fcee811cc04dd395a2af0bd698b1c94
Axel Olsson <axel.olsson@gmail.com>
2021-12-27 00:51:06 +0100

    Part 2

--- Day22.c
+++ Day22.c
@@ -1,7 +1,6 @@
 #include "Helpers.c"
 
 #define CAP 500
-#define CUBES_CAP 600000
 
 typedef struct {
     int x1;
@@ -17,13 +16,7 @@ typedef struct {
     Cuboid cuboid;
 } Step;
 
-typedef struct {
-    int x;
-    int y;
-    int z;
-} Cube;
-
-bool on[200][200][200] = {false};
+static bool grid[900][900][900];
 
 static int parse(const char *input, Step steps[CAP]) {
     char s[5] = {0};
@@ -55,121 +48,121 @@ static inline int min(int a, int b) {
     return a < b ? a : b;
 }
 
-static Cuboid intersection(Cuboid a, Cuboid b) { // TODO.
-    return (Cuboid){max(a.x1, b.x1),
-                    min(a.x2, b.x2),
-                    max(a.y1, b.y1),
-                    min(a.y2, b.y2),
-                    max(a.z1, b.z1),
-                    min(a.z2, b.z2)};
-}
-
-static void dumpCuboid(Cuboid c) {
-    printf("x: (%d,%d), y: (%d,%d), z: (%d,%d)\n", c.x1, c.x2, c.y1, c.y2, c.z1, c.z2);
-}
-
-static void turnOn(Cuboid cuboid) {
-    int x1 = max(cuboid.x1, -50);
-    int x2 = min(cuboid.x2, 50);
-
-    int y1 = max(cuboid.y1, -50);
-    int y2 = min(cuboid.y2, 50);
-
-    int z1 = max(cuboid.z1, -50);
-    int z2 = min(cuboid.z2, 50);
-
-    for (int x = x1; x <= x2; ++x) {
-        for (int y = y1; y <= y2; ++y) {
-            for (int z = z1; z <= z2; ++z) {
-                // if (x < -50 || y < -50 || z < -50) {
-                //     continue;
-                // }
-
-                // if (x > 50 || y > 50 || z > 50) {
-                //     continue;
-                // }
-
-                on[x + 50][y + 50][z + 50] = true;
-            }
-        }
-    }
-}
-
-static void turnOff(Cuboid cuboid) {
-    int x1 = max(cuboid.x1, -50);
-    int x2 = min(cuboid.x2, 50);
-
-    int y1 = max(cuboid.y1, -50);
-    int y2 = min(cuboid.y2, 50);
-
-    int z1 = max(cuboid.z1, -50);
-    int z2 = min(cuboid.z2, 50);
-
-    for (int x = x1; x <= x2; ++x) {
-        for (int y = y1; y <= y2; ++y) {
-            for (int z = z1; z <= z2; ++z) {
-                on[x + 50][y + 50][z + 50] = false;
-            }
-        }
-    }
-}
-
 static int partOne(int n, const Step steps[n]) {
-    assert(n > 0);
+    memset(grid, false, sizeof(grid));
 
     for (int step = 0; step < n; ++step) {
-        printf("On step: %d\n", step + 1);
-        steps[step].on
-            ? turnOn(steps[step].cuboid)
-            : turnOff(steps[step].cuboid);
+        Cuboid cuboid = steps[step].cuboid;
+
+        int x1 = max(cuboid.x1, -50);
+        int y1 = max(cuboid.y1, -50);
+        int z1 = max(cuboid.z1, -50);
+
+        int x2 = min(cuboid.x2, 50);
+        int y2 = min(cuboid.y2, 50);
+        int z2 = min(cuboid.z2, 50);
+
+        for (int x = x1; x <= x2; ++x) {
+            for (int y = y1; y <= y2; ++y) {
+                for (int z = z1; z <= z2; ++z) {
+                    grid[x + 50][y + 50][z + 50] = steps[step].on;
+                }
+            }
+        }
     }
 
     int nOn = 0;
-    for (int x = 0; x < 200; ++x) {
-        for (int y = 0; y < 200; ++y) {
-            for (int z = 0; z < 200; ++z) {
-                if (on[x][y][z]) {
+
+    for (int x = 0; x < 101; ++x) {
+        for (int y = 0; y < 101; ++y) {
+            for (int z = 0; z < 101; ++z) {
+                if (grid[x][y][z]) {
                     ++nOn;
                 }
             }
         }
     }
 
-    printf("nOn: %d\n", nOn);
-
     return nOn;
 }
 
-// 2758514936282235
-// vs
-// 11560337735628470 (all on sum)
+static int compareInt(const void *a, const void *b) {
+    return *(const int32_t *)a - *(const int32_t *)b;
+}
 
-static int partTwo(int n, const Step steps[n]) {
-    uint64_t maxVolume = 0;
-    uint64_t sumVolume = 0;
+static inline uint32_t find(uint32_t n, const int32_t cs[n], int32_t c) {
+    for (uint32_t i = 0; i < n; ++i) {
+        if (cs[i] == c) {
+            return i;
+        }
+    }
+    assert(false && "Could not find coordinate");
+}
+
+// Inspired by https://www.youtube.com/watch?v=YKpViLcTp64, using cooridnate compression.
+static int64_t partTwo(int n, const Step steps[n]) {
+    int32_t xs[850];
+    int32_t ys[850];
+    int32_t zs[850];
+    uint32_t m = 0;
 
     for (int i = 0; i < n; ++i) {
         Cuboid c = steps[i].cuboid;
 
-        uint64_t x = abs(c.x2 - c.x1);
-        uint64_t y = abs(c.y2 - c.y1);
-        uint64_t z = abs(c.z2 - c.z1);
+        xs[m] = c.x1;
+        ys[m] = c.y1;
+        zs[m++] = c.z1;
 
-        if (steps[i].on) {
-            uint64_t volume = x * y * z;
+        // Don't fully grasp why + 1.
+        xs[m] = c.x2 + 1;
+        ys[m] = c.y2 + 1;
+        zs[m++] = c.z2 + 1;
 
-            sumVolume += volume;
+        assert(m < 850);
+    }
 
-            if (volume > maxVolume)
-                maxVolume = volume;
-            printf("%i, volume: %llu\n", i + 1, volume);
+    qsort(xs, m, sizeof(xs[0]), compareInt);
+    qsort(ys, m, sizeof(ys[0]), compareInt);
+    qsort(zs, m, sizeof(zs[0]), compareInt);
+
+    memset(grid, false, sizeof(grid));
+
+    for (int i = 0; i < n; ++i) {
+        Cuboid c = steps[i].cuboid;
+
+        uint32_t ix1 = find(m, xs, c.x1);
+        uint32_t iy1 = find(m, ys, c.y1);
+        uint32_t iz1 = find(m, zs, c.z1);
+
+        uint32_t ix2 = find(m, xs, c.x2 + 1);
+        uint32_t iy2 = find(m, ys, c.y2 + 1);
+        uint32_t iz2 = find(m, zs, c.z2 + 1);
+
+        // Don't fully grasp why + 1 and < instead of no + 1 and <=.
+        for (uint32_t iz = iz1; iz < iz2; ++iz) {
+            for (uint32_t iy = iy1; iy < iy2; ++iy) {
+                for (uint32_t ix = ix1; ix < ix2; ++ix) {
+                    grid[iz][iy][ix] = steps[i].on;
+                }
+            }
         }
     }
 
-    printf("max volume: %llu\n", maxVolume);
-    printf("sum volume: %llu\n", sumVolume);
+    int64_t sumVolume = 0;
 
-    return -1;
+    for (uint32_t iz = 0; iz < m; ++iz) {
+        for (uint32_t iy = 0; iy < m; ++iy) {
+            for (uint32_t ix = 0; ix < m; ++ix) {
+                if (grid[iz][iy][ix]) {
+                    sumVolume += (int64_t)(xs[ix + 1] - xs[ix]) *
+                                 (int64_t)(ys[iy + 1] - ys[iy]) *
+                                 (int64_t)(zs[iz + 1] - zs[iz]);
+                }
+            }
+        }
+    }
+
+    return sumVolume;
 }
 
 int main() {
@@ -178,13 +171,13 @@ int main() {
     Step steps[CAP] = {0};
     int n = parse(input, steps);
 
-    // Helpers_assert(PART1, Helpers_clock(),
-    //                partOne(n, steps),
-    //                590784, 542711);
+    Helpers_assert(PART1, Helpers_clock(),
+                   partOne(n, steps),
+                   474140, 542711);
 
     Helpers_assert(PART2, Helpers_clock(),
                    partTwo(n, steps),
-                   -1, -2);
+                   2758514936282235, 1160303042684776);
 
     return 0;
 }
