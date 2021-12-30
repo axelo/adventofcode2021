b50b32e690044110281eb2089b0259e7bb31c8c3
Axel Olsson <axel.olsson@gmail.com>
2021-12-30 14:16:53 +0100

    Part 1

--- Day23.c
+++ Day23.c
@@ -16,12 +16,13 @@ typedef struct {
 } Amp;
 
 typedef struct {
+    int n;
     Amp as[8];
 } Amps;
 
 typedef struct {
     int energy;
-    int amp[2];
+    Amp amp;
     char m[5][13];
 } Map;
 
@@ -92,10 +93,10 @@ static Map parse(const char *input) {
 }
 
 static void dump(Map map) {
-    int hx = map.amp[0];
-    int hy = map.amp[1];
+    int hx = map.amp.x;
+    int hy = map.amp.y;
 
-    printf("Energy used: %d\n", map.energy);
+    // printf("Energy used: %d\n", map.energy);
 
     for (int y = 0; y < 5; ++y) {
         for (int x = 0; x < 13; ++x) {
@@ -193,17 +194,39 @@ static void dump(Map map) {
 //     return 0;
 // }
 
-static Amps ampsFromMap(Map map) {
+static Amps findMalplacedAmps(Map map) {
     Amps amps = {0};
-    int n = 0;
 
-    for (int y = 0; y < 5; ++y) {
-        for (int x = 0; x < 13; ++x) {
-            if (map.m[y][x] >= 'A' && map.m[y][x] <= 'D') {
-                // amps.as[n].t = map.m[y][x];
-                amps.as[n].x = x;
-                amps.as[n].y = y;
-                ++n;
+    for (int x = 1; x < 12; ++x) {
+        if (map.m[1][x] >= 'A' && map.m[1][x] <= 'D') {
+            amps.as[amps.n].x = x;
+            amps.as[amps.n].y = 1;
+            ++amps.n;
+        }
+    }
+
+    for (int x = 1; x < 12; ++x) {
+        if (map.m[2][x] >= 'A' && map.m[2][x] <= 'D') {
+            if ((x == 3 && (map.m[3][x] != 'A' || map.m[2][x] != 'A')) ||
+                (x == 5 && (map.m[3][x] != 'B' || map.m[2][x] != 'B')) ||
+                (x == 7 && (map.m[3][x] != 'C' || map.m[2][x] != 'C')) ||
+                (x == 9 && (map.m[3][x] != 'D' || map.m[2][x] != 'D'))) {
+                amps.as[amps.n].x = x;
+                amps.as[amps.n].y = 2;
+                ++amps.n;
+            }
+        }
+    }
+
+    for (int x = 1; x < 12; ++x) {
+        if (map.m[3][x] >= 'A' && map.m[3][x] <= 'D') {
+            if ((x == 3 && (map.m[3][x] != 'A')) ||
+                (x == 5 && (map.m[3][x] != 'B')) ||
+                (x == 7 && (map.m[3][x] != 'C')) ||
+                (x == 9 && (map.m[3][x] != 'D'))) {
+                amps.as[amps.n].x = x;
+                amps.as[amps.n].y = 3;
+                ++amps.n;
             }
         }
     }
@@ -212,11 +235,11 @@ static Amps ampsFromMap(Map map) {
 }
 
 static Maps moveOnce(Map map) {
-    printf("moveOnce from:\n");
-    dump(map);
+    // printf("moveOnce from:\n");
+    // dump(map);
 
-    int x = map.amp[0];
-    int y = map.amp[1];
+    int x = map.amp.x;
+    int y = map.amp.y;
 
     bool isInHallway = y == 1;
 
@@ -241,8 +264,8 @@ static Maps moveOnce(Map map) {
 
     Maps maps = {0};
 
-    printf("(%d,%d): %c, in hallway? %s. In room? %s. In correct room? %s\n", x, y, t,
-           isInHallway ? "true" : "false", isInRoom ? "true" : "false", isInCorrectRoom ? "true" : "false");
+    // printf("(%d,%d): %c, in hallway? %s. In room? %s. In correct room? %s\n", x, y, t,
+    //        isInHallway ? "true" : "false", isInRoom ? "true" : "false", isInCorrectRoom ? "true" : "false");
 
     if (isInCorrectRoom) {
         if (y == 3 || (map.m[3][x] == t)) {
@@ -263,7 +286,7 @@ static Maps moveOnce(Map map) {
             }
         }
 
-        printf("Can we go to the hallway? %s\n", empty ? "true" : "false");
+        // printf("Can we go to the hallway? %s\n", empty ? "true" : "false");
 
         if (empty) {
             // yes, can we past the hallway to the correct room?
@@ -286,14 +309,30 @@ static Maps moveOnce(Map map) {
             if (empty) {
                 // yes, can we enter the room?
                 if (map.m[3][destX] == 0) {
-                    assert(false);
+                    // Bottom of room.
+                    maps.ms[maps.n] = map;
+
+                    maps.ms[maps.n].energy += energyPerStep * (abs(destX - x) + ySteps + 2);
+
+                    maps.ms[maps.n].amp.x = destX;
+                    maps.ms[maps.n].amp.y = 3;
+
+                    maps.ms[maps.n].m[y][x] = 0;
+                    maps.ms[maps.n].m[3][destX] = t;
+
+                    ++maps.n;
+                    assert(maps.n < 300);
+
+                    return maps;
+
                 } else if (map.m[3][destX] == t && map.m[2][destX] == 0) {
+                    // Top of room.
                     maps.ms[maps.n] = map;
 
                     maps.ms[maps.n].energy += energyPerStep * (abs(destX - x) + ySteps + 1);
 
-                    maps.ms[maps.n].amp[0] = destX;
-                    maps.ms[maps.n].amp[1] = 2;
+                    maps.ms[maps.n].amp.x = destX;
+                    maps.ms[maps.n].amp.y = 2;
 
                     maps.ms[maps.n].m[y][x] = 0;
                     maps.ms[maps.n].m[2][destX] = t;
@@ -315,13 +354,13 @@ static Maps moveOnce(Map map) {
                     continue;
                 }
 
-                printf("Could try (%d,%d)\n", x2, 1);
+                // printf("Could try (%d,%d)\n", x2, 1);
                 maps.ms[maps.n] = map;
 
                 maps.ms[maps.n].energy += energyPerStep * (abs(x2 - x) + ySteps);
 
-                maps.ms[maps.n].amp[0] = x2;
-                maps.ms[maps.n].amp[1] = 1;
+                maps.ms[maps.n].amp.x = x2;
+                maps.ms[maps.n].amp.y = 1;
 
                 maps.ms[maps.n].m[y][x] = 0;
                 maps.ms[maps.n].m[1][x2] = t;
@@ -338,13 +377,13 @@ static Maps moveOnce(Map map) {
                     continue;
                 }
 
-                printf("Could try (%d,%d)\n", x2, 1);
+                // printf("Could try (%d,%d)\n", x2, 1);
                 maps.ms[maps.n] = map;
 
                 maps.ms[maps.n].energy += energyPerStep * (abs(x2 - x) + ySteps);
 
-                maps.ms[maps.n].amp[0] = x2;
-                maps.ms[maps.n].amp[1] = 1;
+                maps.ms[maps.n].amp.x = x2;
+                maps.ms[maps.n].amp.y = 1;
 
                 maps.ms[maps.n].m[y][x] = 0;
                 maps.ms[maps.n].m[1][x2] = t;
@@ -355,7 +394,7 @@ static Maps moveOnce(Map map) {
             return maps;
         } else {
             // no, can't go to the hallway
-            printf("Stuck in wrong room!");
+            // printf("Stuck in wrong room!\n");
             return maps;
         }
     }
@@ -381,18 +420,18 @@ static Maps moveOnce(Map map) {
                 }
             }
         }
-        printf("xEmpty to %d? %s\n", destX, xEmpty ? "true" : "false");
+        // printf("xEmpty to %d? %s\n", destX, xEmpty ? "true" : "false");
 
         if (xEmpty) {
             if (map.m[3][destX] == 0) {
-                printf("Could try %d,%d\n", 3, destX);
+                // printf("Could try %d,%d\n", 3, destX);
 
                 maps.ms[maps.n] = map;
 
                 maps.ms[maps.n].energy += energyPerStep * (abs(destX - x) + abs(3 - y));
 
-                maps.ms[maps.n].amp[0] = destX;
-                maps.ms[maps.n].amp[1] = 3;
+                maps.ms[maps.n].amp.x = destX;
+                maps.ms[maps.n].amp.y = 3;
 
                 maps.ms[maps.n].m[y][x] = 0;
                 maps.ms[maps.n].m[3][destX] = t;
@@ -402,14 +441,14 @@ static Maps moveOnce(Map map) {
 
                 return maps;
             } else if (map.m[3][destX] == t && map.m[2][destX] == 0) {
-                printf("Could try %d,%d\n", 2, destX);
+                // printf("Could try %d,%d\n", 2, destX);
 
                 maps.ms[maps.n] = map;
 
                 maps.ms[maps.n].energy += energyPerStep * (abs(destX - x) + abs(2 - y));
 
-                maps.ms[maps.n].amp[0] = destX;
-                maps.ms[maps.n].amp[1] = 2;
+                maps.ms[maps.n].amp.x = destX;
+                maps.ms[maps.n].amp.y = 2;
 
                 maps.ms[maps.n].m[y][x] = 0;
                 maps.ms[maps.n].m[2][destX] = t;
@@ -419,84 +458,123 @@ static Maps moveOnce(Map map) {
 
                 return maps;
             } else {
-                printf("Can't move into correct room\n");
+                // printf("Can't move into correct room\n");
                 return maps;
             }
         }
 
-        assert(false);
+        return maps;
     }
 
     return maps;
 }
 
-static int64_t partOne(Map map) {
+static int64_t ccc = 0;
+
+static int64_t partOne(Map map, int64_t energy) {
+    Amps amps = findMalplacedAmps(map);
+
+    if (amps.n == 0) {
+        // printf("** All done! **\n");
+        // dump(map);
+        // printf("****\n");
+        ++ccc;
+
+        if ((ccc % 20000) == 0) {
+            printf("Done: %lld - %d\n", ccc, map.energy);
+        }
+        return map.energy < energy ? map.energy : energy;
+    }
+
+    for (int i = 0; i < amps.n; ++i) {
+        // printf("\nMalplaced amp: (%d,%d): %c\n",
+        //        amps.as[i].x, amps.as[i].y,
+        //        map.m[amps.as[i].y][amps.as[i].x]);
+
+        Map map2 = map;
+        map2.amp = amps.as[i];
+
+        Maps maps = moveOnce(map2);
+
+        // printf("Possible new maps: %d\n", maps.n);
+
+        for (int j = 0; j < maps.n; ++j) {
+            int64_t e2 = partOne(maps.ms[j], energy);
+
+            if (e2 < energy) {
+                energy = e2;
+            }
+        }
+    }
+
+    return energy;
+
     // Move middle B
-    map.amp[0] = 7;
-    map.amp[1] = 2;
+    map.amp.x = 7;
+    map.amp.y = 2;
     Maps maps = moveOnce(map);
     dump(maps.ms[1]);
 
     // Move middle C
     Map map2 = maps.ms[1];
-    map2.amp[0] = 5;
-    map2.amp[1] = 2;
+    map2.amp.x = 5;
+    map2.amp.y = 2;
     Maps maps2 = moveOnce(map2);
     dump(maps2.ms[0]);
 
     // Move middle D
     Map map3 = maps2.ms[0];
-    map3.amp[0] = 5;
-    map3.amp[1] = 3;
+    map3.amp.x = 5;
+    map3.amp.y = 3;
     Maps maps3 = moveOnce(map3);
     dump(maps3.ms[0]);
 
     // Move B again.
     Map map4 = maps3.ms[0];
-    map4.amp[0] = 4;
-    map4.amp[1] = 1;
+    map4.amp.x = 4;
+    map4.amp.y = 1;
     Maps maps4 = moveOnce(map4);
     dump(maps4.ms[0]);
 
     // Move other B.
     Map map5 = maps4.ms[0];
-    map5.amp[0] = 3;
-    map5.amp[1] = 2;
+    map5.amp.x = 3;
+    map5.amp.y = 2;
     Maps maps5 = moveOnce(map5);
     dump(maps5.ms[0]);
 
     // Move right D.
     Map map6 = maps5.ms[0];
-    map6.amp[0] = 9;
-    map6.amp[1] = 2;
+    map6.amp.x = 9;
+    map6.amp.y = 2;
     Maps maps6 = moveOnce(map6);
     dump(maps6.ms[0]);
 
     // Move right A.
     Map map7 = maps6.ms[0];
-    map7.amp[0] = 9;
-    map7.amp[1] = 3;
+    map7.amp.x = 9;
+    map7.amp.y = 3;
     Maps maps7 = moveOnce(map7);
     dump(maps7.ms[0]);
 
     // Move right hallway D.
     Map map8 = maps7.ms[0];
-    map8.amp[0] = 8;
-    map8.amp[1] = 1;
+    map8.amp.x = 8;
+    map8.amp.y = 1;
     Maps maps8 = moveOnce(map8);
     dump(maps8.ms[0]);
 
     // Move other hallway D.
     Map map9 = maps8.ms[0];
-    map9.amp[0] = 6;
-    map9.amp[1] = 1;
+    map9.amp.x = 6;
+    map9.amp.y = 1;
     Maps maps9 = moveOnce(map9);
     dump(maps9.ms[0]);
 
     // Move hallway A.
     Map map10 = maps9.ms[0];
-    map10.amp[0] = 10;
-    map10.amp[1] = 1;
+    map10.amp.x = 10;
+    map10.amp.y = 1;
     Maps maps10 = moveOnce(map10);
     dump(maps10.ms[0]);
 
@@ -516,7 +594,7 @@ int main() {
     Map map = parse(input);
 
     Helpers_assert(PART1, Helpers_clock(),
-                   partOne(map),
+                   partOne(map, INT_MAX),
                    -1, -2);
 
     // Helpers_assert(PART2, Helpers_clock(),
